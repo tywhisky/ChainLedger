1760857a75c78d0d3667ef1804f4cd1d7c4629c2
Taine Fan <fty221@gmail.com>
2026-07-17 16:27:16 +0800

    Fix contract name typo and update constructor parameters for FreelanceEscrow

--- FreelanceEscrow.sol
+++ FreelanceEscrow.sol
@@ -2,7 +2,7 @@
 // compiler version must be greater than or equal to 0.8.26 and less than 0.9.0
 pragma solidity ^0.8.26;
 
-contract FreelaceEscrow {
+contract FreelanceEscrow {
     address public immutable buyer;
     address public immutable seller; 
     address public immutable arbiter;
@@ -11,17 +11,21 @@ contract FreelaceEscrow {
     uint256 public immutable reviewPeriod;
     uint256 public immutable arbitrationPeriod;
 
+    uint256 public immutable depositAmount;
+
     constructor(address _seller, 
                 address _arbiter, 
-                uint _deliveryDeadline, 
-                uint _reviewPeriod, 
-                uint _arbitrationPeriod) {
+                uint256 _deliveryDeadline, 
+                uint256 _reviewPeriod, 
+                uint256 _arbitrationPeriod,
+                uint256 _depositAmount) {
         require(_seller != address(0), "Not valid address of seller.");
         require(_arbiter != address(0), "Not valid address of arbiter.");
         require(msg.sender != _seller && _seller != _arbiter && _arbiter != msg.sender);
-        require(_deliveryDeadline <= block.timestamp);
+        require(_deliveryDeadline > block.timestamp);
         require(_reviewPeriod != 0);
         require(_arbitrationPeriod != 0);
+        require(_depositAmount > 0);
 
         buyer = msg.sender;
         seller = _seller;
@@ -29,5 +33,6 @@ contract FreelaceEscrow {
         deliveryDeadline = _deliveryDeadline;
         reviewPeriod = _reviewPeriod;
         arbitrationPeriod = _arbitrationPeriod;
+        depositAmount = _depositAmount;
     }
 }
